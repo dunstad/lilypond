b26a242f553c9d76d7d0643cc1c490cff37699b4
dunstad <anthony00.blount00@gmail.com>
2019-11-22 13:56:02 +0900

    anki output kinda works?

--- anki.py
+++ anki.py
@@ -1,31 +1,37 @@
 # this is just the example code from https://github.com/kerrickstaley/genanki
 
+import genanki
+
 my_model = genanki.Model(
   1091735104,
   'Simple Model with Media',
   fields=[
-    {'name': 'Question'},
-    {'name': 'Answer'},
-    {'name': 'MyMedia'},                                  # ADD THIS
+    {'name': 'Score'},
+    {'name': 'EasyScore'},
+    {'name': 'Music'},
   ],
   templates=[
     {
       'name': 'Card 1',
-      'qfmt': '{{Question}}<br>{{MyMedia}}',              # AND THIS
-      'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+      'qfmt': '{{Score}}',
+      'afmt': '{{FrontSide}}<hr id="answer">{{EasyScore}}<br>{{Music}}',
     },
   ])
 
 my_note = genanki.Note(
   model=my_model,
-  fields=['Capital of Argentina', 'Buenos Aires'])
+  fields=['dango.preview.png', 'dango-1.preview.png', 'dango-1.wav'])
 
 my_deck = genanki.Deck(
   2059400110,
-  'Country Capitals')
+  'Sight reading')
 
 my_deck.add_note(my_note)
 
 my_package = genanki.Package(my_deck)
-my_package.media_files = ['sound.mp3', 'images/image.jpg']
+my_package.media_files = [
+  'dango/dango.preview.png',
+  'dango/dango-1.preview.png',
+  'dango/dango-1.wav',
+]
 my_package.write_to_file('output.apkg')
